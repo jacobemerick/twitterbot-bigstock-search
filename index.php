f76029dea0e4e19b331f80a27b20c4d6d4c74b6f
Jacob Emerick <jpemeric@gmail.com>
2015-01-25 11:23:07 -0700

    semi-intelligent (hacked) detection of multiple user mentions

--- index.php
+++ index.php
@@ -30,7 +30,26 @@ while (!$stream->feof()) {
         $message = json_decode($message, true);
         if (isset($message['in_reply_to_screen_name']) && $message['in_reply_to_screen_name'] === 'fetchmeaphoto') {
             $query = $message['text'];
-            $query = substr($message['text'], 15);
+            $user_mentions = [$message['user']['screen_name']];
+            foreach ($message['entities'] as $entity_type => $entities) {
+                foreach ($entities as $entity) {
+                    $length = $entity['indices'][1] - $entity['indices'][0];
+                    $query = substr_replace(
+                        $query,
+                        str_repeat('?', $length),
+                        $entity['indices'][0],
+                        $length
+                    );
+                    if ($entity_type == 'user_mentions' && $entity['screen_name'] != 'fetchmeaphoto') {
+                        $user_mentions[] = $entity['screen_name'];
+                    }
+                }
+            }
+            $query = str_replace('?', '', $query);
+            $query = trim($query);
+            if (empty($query)) {
+                $query = '*';
+            }
 
             include_once __DIR__ . '/vendor/jacobemerick/bigstock-api-services/src/service/SearchService.php';
             $bigstock_request = new BigstockAPI\Service\SearchService('API_ACCOUNT');
@@ -50,8 +69,10 @@ while (!$stream->feof()) {
 
             if ($response->response_code = 200 && $response->message == 'success') {
                 $image_id = rand(0, ($response->data->paging->items - 1));
-                $return_message = '';
-                $return_message .= ".@{$message['user']['screen_name']} ";
+                $return_message = '.';
+                foreach ($user_mentions as $user) {
+                    $return_message .= "@{$user} ";
+                }
                 $return_message .= substr($response->data->images[$image_id]->title, 0, 50);
                 $return_message .= " http://www.bigstockphoto.com/image-{$response->data->images[$image_id]->id}/";
 
@@ -64,8 +85,10 @@ while (!$stream->feof()) {
                     'in_reply_to_status_id' => $message['id_str'],
                 ))->addPostFile('media', $return_image);
             } else {
-                $return_message = '';
-                $return_message .= ".@{$message['user']['screen_name']} ";
+                $return_message = '.';
+                foreach ($user_mentions as $user) {
+                    $return_message .= "@{$user}";
+                }
                 $return_message .= "sorry, I did not find an image for your search ({$query}). Please try again :(";
                 $return_image = '';
 
